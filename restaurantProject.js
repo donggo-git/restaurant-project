008feb4d6a7948b98bcd60cb8bebd07737ed210a
Dong <dlttthvlhhth@gmail.com>
2021-01-01 21:10:01 -0800

    add item-detail-active

--- restaurantProject.js
+++ restaurantProject.js
@@ -1,33 +1,42 @@
 $(document).ready(function () {
-    let x = 0;
+    let move = 0;
     const y = -30;
+    let detail_isActive = false;
     $(".next").click(function () {
 
-        x += 1;
+        move += 1;
         $('.slide').css('transition', 'all 0.5s')
-        $('.slide').css('transform', `translateX(${-x * 30 + y}vw)`)
+        $('.slide').css('transform', `translateX(${-move * 30 + y}vw)`)
 
     });
     $('.slide').on('transitionend', function () {
-        if (x == 3) {
-            x = 0
+        if (move == 3) {
+            move = 0
             $('.slide').css('transition', 'none');
-            $('.slide').css('transform', `translateX(${-x * 30 + y}vw)`)
+            $('.slide').css('transform', `translateX(${-move * 30 + y}vw)`)
         }
-        else if (x == -1) {
-            x = 2;
+        else if (move == -1) {
+            move = 2;
             $('.slide').css('transition', 'none');
-            $('.slide').css('transform', `translateX(${-x * 30 + y}vw)`);
+            $('.slide').css('transform', `translateX(${-move * 30 + y}vw)`);
 
         }
     })
 
     $(".previous").click(function () {
-        x -= 1;
+        move -= 1;
         $('.slide').css('transition', 'all 0.5s')
-        $('.slide').css('transform', `translateX(${-x * 30 + y}vw)`)
+        $('.slide').css('transform', `translateX(${-move * 30 + y}vw)`)
 
     });
-
+    $('.item-name').click(function () {
+        detail_isActive = !detail_isActive;
+        if (detail_isActive) {
+            $(this).parent().children('.item-deital').addClass('item-deital-active')
+        }
+        else {
+            $(this).parent().children('.item-deital').removeClass('item-deital-active')
+        }
+    })
 
 })
